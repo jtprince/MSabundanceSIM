b3bb6b6aac713fa3a38dc7fc9d50891aad4c876f
John T. Prince <jtprince@gmail.com>
2017-01-18 00:39:37 -0700

    fix major type error
    The type was clearly meant to be modulated based on the indices of the
    differentially expressed proteins.  It was locking into a 'control'
    state and never hitting the case state for the proteins based on the
    various internal toggle logic.
    
    Also, stripped trailing whitespace.

--- msabundancesim.rb
+++ msabundancesim.rb
@@ -18,16 +18,16 @@ control_n = 5 						# how many control samples to generate
 
 case_n = 5 								# how many case samples to generate
 
-diff_express_percent = 3 	# what percent of proteins to differentially express 
+diff_express_percent = 3 	# what percent of proteins to differentially express
 													# between case and control
 
-control_variance = 1	 		# Variance for control samples (max lambda for Poisson distribution). 
+control_variance = 1	 		# Variance for control samples (max lambda for Poisson distribution).
 													# The higher the value, the more fold change will occur
-											 		# among healthy populations. Used only when multiple 
+											 		# among healthy populations. Used only when multiple
 													# abundances are not provided in master fasta. Modification
 													# not recommended.
 
-case_variance = 2 				# Variance increase for case samples (max lambda for Poisson distribution). 													# The higher the value, the more fold change will occur. 
+case_variance = 2 				# Variance increase for case samples (max lambda for Poisson distribution). 													# The higher the value, the more fold change will occur.
 
 ##########################
 
@@ -45,7 +45,7 @@ end
 
 def inverse_transform_sample(lambda)
 	max = poisson(lambda, lambda)
-	y = rand * (max) 
+	y = rand * (max)
 	ks = (0..lambda*4).to_a.shuffle # attempt to capture the entire distribution without wasting tests
 	ks.each do |k|
 		p_k = poisson(lambda, k)
@@ -57,11 +57,11 @@ end
 def get_fold_change_clean(a_i, lambda)
 	# max fold change at lowest abundance
 	raw_fc = inverse_transform_sample(lambda)
-	norm_fc = raw_fc 
+	norm_fc = raw_fc
 	sign = [1,-1].sample
 	return norm_fc * sign
 end
- 
+
 def get_fold_change(a_i, lambda)
 	# max fold change at lowest abundance
 	raw_fc = inverse_transform_sample(lambda)
@@ -96,7 +96,7 @@ $abundance_min = 9999999999999999999
 while line = master_fasta.gets
 	line = line.chop
 	if line.index(">") != nil #first line of entry
-		
+
 		unless abundances.size == 0 # first time
 			abundances.sort!
 
@@ -125,28 +125,30 @@ proteins << [entries,abundances]
 
 # generate which proteins will be differentially expressed
 diff_expressed_ids = (0..proteins.size-1).to_a.sample((proteins.size * diff_express_percent/100.0).to_i)
-diff_expressed_signs = Array.new(diff_expressed_ids.size){[-1,1].sample}
+#diff_expressed_signs = Array.new(diff_expressed_ids.size){[-1,1].sample}
 
 sample_n = case_n + control_n
 (0...sample_n).each do |n| # for each sample
-	type = "control"
+	sample_type = "control"
 	if n < case_n # make a case sample
-		type = "case"
+		sample_type = "case"
 	end
 	puts "Creating sample #{n} of #{sample_n}"
 
 	# create output file
-	outfile = File.open("#{n}_#{type}","w")
+	outfile = File.open("#{n}_#{sample_type}","w")
 	proteins.each_with_index do |protein, idx|
+    protein_type = sample_type
 		# put first line of fasta with simulated abundance
 		sign = [1,-1].sample
-		if type=='case' and diff_expressed_ids.index(idx) != nil
-			sign = diff_expressed_signs[idx]
+		if sample_type=='case' and diff_expressed_ids.index(idx) != nil
+      protein_type = 'case'
+			#sign = diff_expressed_signs[idx]
 		else
-			type = 'control'
+      protein_type = 'control'
 		end
 
-		outfile.puts "#{protein[0][0]} + ##{sample_abundance(protein[1], get_fold_change(protein[1], type=='control' ? control_variance : case_variance))}" 
+		outfile.puts "#{protein[0][0]} + ##{sample_abundance(protein[1], get_fold_change(protein[1], protein_type=='control' ? control_variance : case_variance))}"
 		protein[0][1..-1].each do |additional_line|
 			outfile.puts additional_line
 		end
